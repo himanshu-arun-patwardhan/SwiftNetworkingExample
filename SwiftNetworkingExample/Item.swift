106fdaaa506d6c987bd1cb70475091733fb142f2
Himanshu Arun Patwardhan <himanshu.arun.patwardhan@gmail.com>
2025-11-20 18:18:08 +0530

    [project] - basic project structure

---
+++ SwiftNetworkingExample/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SwiftNetworkingExample
+//
+//  Created by Himanshu Arun Patwardhan
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SwiftNetworkingExampleTests/SwiftNetworkingExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNetworkingExampleTests.swift
+//  SwiftNetworkingExampleTests
+//
+//  Created by Himanshu Arun Patwardhan
+//
+
+import Testing
+@testable import SwiftNetworkingExample
+
+struct SwiftNetworkingExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
